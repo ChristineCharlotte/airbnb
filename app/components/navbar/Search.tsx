a6b8f0b32383e618bc4f43e538bf2322cfe78fd6
Romance Ucely <faaa@live.com>
2023-05-03 19:39:17 +0800

    init commit [0:00-20:00]

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+const Search = () => {
+  return (
+    <div>
+      <div
+        className="
+      border-[1px]
+      w-full
+      md:w-auto
+      py-2
+      rounded-full
+      shadow-sm
+      hover:shadow-md
+      transition
+      cursor-pointer
+      "
+      ></div>
+    </div>
+  );
+};
+
+export default Search;
